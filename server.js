ffcba399e08f6699f11ddbc3653cb02e3a3a33c9
alieuroshub001 <alirayyan.euroshub@gmail.com>
2025-07-11 15:18:36 +0500

    fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'src')));
 
 // Password check endpoint
-const checkPassword = require('./src/api/check-password');
+const checkPassword = require('./api/check-password');
 app.post('/api/check-password', checkPassword);
 
 // Serve index.html for all other routes
